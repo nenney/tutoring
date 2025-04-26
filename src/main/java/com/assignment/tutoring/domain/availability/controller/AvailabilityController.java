d54be393fbe001aa22c2d14f7ef059b277722644
yeonhwan <a01056499749@gmail.com>
2025-04-26 23:26:57 +0900

    fix: API 엔드포인트 수정

--- src/main/java/com/assignment/tutoring/domain/availability/controller/AvailabilityController.java
+++ src/main/java/com/assignment/tutoring/domain/availability/controller/AvailabilityController.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/availabilities")
+@RequestMapping("/api/availabilities")
 @RequiredArgsConstructor
 public class AvailabilityController {
 
